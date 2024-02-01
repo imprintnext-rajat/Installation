e476ed6f6ecee75707f01c3f2c9abe25df7e7420
imprintnext-rajat <rajat@imprintnext.com>
2024-02-01 17:31:20 +0530

    modify deploymeny file

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-apt update 
+apt update -y
 ## jenkins image (jenkins/jenkins:2.391-jdk11)
 apt install -y php7.4
-# install nodejs 
-sudo apt install nodejs
-#check version 
-node -v
- 
- #install  composer
- sudo apt update
- sudo apt install php-cli unzip
+# install nodejs
+apt install nodejs -y
+apt install nano -y
+#check version
+#node -v
 
- cd ~
+ #install  composer
+ apt update -y
+ apt install -y php-cli unzip
+
+cd ~
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
 HASH=`curl -sS https://composer.github.io/installer.sig`
 echo $HASH
 php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+ php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 #check version
-composer
+#composer
